9593698bce37b2aef560cf36ad1c4cc5ad34cb8f
chqwer2 <ch981129@126.com>
2023-03-01 21:07:00 +0800

    test

--- SSL_Sound/run.sh
+++ SSL_Sound/run.sh
@@ -14,3 +14,5 @@ YOUR_SAVE_PATH='/bask/projects/j/jiaoj-3d-vision/Hao/VideoHandler/SSL_Sound/save
 # /bask/projects/j/jiaoj-3d-vision/360XProject/Data/Meta/vis.csv
 # visualizing the ITD prediction of videos over time
 ./scripts/visualization_video.sh 'YourVideo' $YOUR_SAVE_PATH
+
+
